7b2b8875a04e22932140c74b2700f8643c6f9f91
Zafor Iqbal <zaforiqbal13@gmail.com>
2022-06-16 07:19:06 +0600

    Create wordpress-functions.php

---
+++ wordpress-functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Gets the current user’s ID if logged in
+ * Ref: https://developer.wordpress.org/reference/functions/get_current_user_id/
+*/
+get_current_user_id();
+
+/* Gets the current user’s ID if logged in
+ * Ref: https://developer.wordpress.org/reference/functions/wp_get_current_user/
+*/
+wp_get_current_user();
